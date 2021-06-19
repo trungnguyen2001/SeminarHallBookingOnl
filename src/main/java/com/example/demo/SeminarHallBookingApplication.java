1320832f149258981c6c84f78f4439c9cf84b274
Phats Lys <phatly2001@gmail.com>
2021-06-19 09:22:21 +0700

    first commit

---
+++ src/main/java/com/example/demo/SeminarHallBookingApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeminarHallBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeminarHallBookingApplication.class, args);
+	}
+
+}
